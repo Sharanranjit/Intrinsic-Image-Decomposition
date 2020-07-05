4bef7c5023db661f38163ed5dd16fb0077a8d8e5
Sharan <sharanranjit@gmail.com>
2020-07-05 13:25:04 +0530

    Syntax Corrections

--- test.py
+++ test.py
@@ -10,12 +10,12 @@ from losses import albedo_loss_function, shading_loss_function
 from utils import predict,load_images
 import tensorflow as tf
 from PIL import Image
+import numpy as np
 
 # Argument Parser
 parser = argparse.ArgumentParser(description='Learning Inverse Rendering using a single image')
 parser.add_argument('--input', type=str, help='Any input image')
 parser.add_argument('--model', type=str, help='Trained Keras model file.')
-parser.add_argument('--output_dir', type=str, help='Directory to save output images.')
 args = parser.parse_args()
 
 #initialize the inputs for loss functions with zero tensors(can by any 3 rank tensor)
@@ -34,19 +34,23 @@ model = load_model(args.model, custom_objects=custom_objects, compile=False)
 
 
 start = time.time()
-#print('Testing...')
-inputs = load_images(args.input)
+print('Testing...')
+inputs = load_images(glob.glob(args.input))
+print(inputs.shape)
 (albedo,shading) = predict(model, inputs)
+print(albedo.shape, shading.shape)
 end = time.time()
 
 #Save images
+albedo = albedo[0]
+shading = shading[0]
 albedo = (255.0 / albedo.max() * (albedo - albedo.min())).astype(np.uint8)
 shading = (255.0 / shading.max() * (shading - shading.min())).astype(np.uint8)
 albedo = Image.fromarray(albedo)
 shading = Image.fromarray(shading)
 
-albedo.save(args.output_dir+"albedo.png")
-shading.save(args.output_dir+"shading.png")
-
+albedo.save(args.input.split('.')[0]+"_albedo.png")
+shading.save(args.input.split('.')[0]+"_shading.png")
+print("Outputs saved in "+args.input.split('.')[0])
 
 print('\nTime taken: ', end-start, 's')
